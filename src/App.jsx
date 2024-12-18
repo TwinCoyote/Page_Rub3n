1a8ddb38848ecec288d43aa3604b4878770fb3b3
TwinCoyote <twincoyote0732@gmail.com>
2024-12-18 16:39:09 -0600

    Add image and react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styles from "./App.module.css";
+import { Navbar } from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className={style.App}>
+      {" "}
+      xd
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
