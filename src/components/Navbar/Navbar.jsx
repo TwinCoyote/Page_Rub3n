0f4eab540b21961e6eb1c39bf97d3799bc6bfd81
TwinCoyote <twincoyote0732@gmail.com>
2024-12-18 17:03:37 -0600

    Add header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
 
 export const Navbar = () => {
   return (
-            <nav>
-                <a href='/'>My portafolio</a>
-            </nav>
-      )
-}
+    <header className="home">
+      <nav>
+        <un className="List">
+          <li>
+            <a href="#Sobre_mi" className="">
+              Sobre Mi
+            </a>
+          </li>
+          <li>
+            <a href="#Proyectos" className="">
+              Proyectos{" "}
+            </a>
+          </li>
+          <li>
+            <a href="#Ventas " className="">
+              Ventas
+            </a>
+          </li>
+          <li>
+            <a href="#Conacto" className="">
+              Contacto{" "}
+            </a>
+          </li>
+        </un>
+      </nav>
+      <div className="container">
+        <div className="slide_1">
+          <span className="texto first-text"> ¡Hola, mundo!</span>
+        </div>
+        <div className="escritura_mov">
+          <span className="escritura"> </span>
+        </div>
+      </div>
+    </header>
+  );
+};
