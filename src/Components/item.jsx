e8e58f05e9924d339c4492cb5f5a888368bc42c9
TwinCoyote <twincoyote0732@gmail.com>
2025-02-01 13:37:52 -0600

    AFTER BOX REACT

---
+++ src/Components/item.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function item() {
+  return <div className=""></div>;
+}
+
+export default item;
---
+++ src/Components/itembox.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function itembox() {
+  return <div>itembox</div>;
+}
+
+export default itembox;
